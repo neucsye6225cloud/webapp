c5d722ef9c9202aebe4b657c862b0645f250ca05
Siddharth Dhanuka <nonsense171@gmail.com>
2024-02-07 15:25:43 -0500

    added requirements file, global headers, fixed authentication, error handling

--- blogapp/__init__.py
+++ blogapp/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_login import LoginManager
-from flask_basicauth import BasicAuth
+from flask_httpauth import HTTPBasicAuth 
 import os
 
 app = Flask(__name__)
@@ -13,17 +13,20 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin@localhost/cloud'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['BCRYPT_HANDLE_LONG_PASSWORDS'] = True
-app.config['BASIC_AUTH_USERNAME'] = 'jane.doe@example.com'
-app.config['BASIC_AUTH_PASSWORD'] = 'skdjfhskdfjhg'
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-basic_auth = BasicAuth(app)
+auth = HTTPBasicAuth()
 
 # to avoid circular recursive imports and import error, import routes after creating app, db objects 
 from blogapp import routes
 
 with app.app_context():
     db.create_all()
+
+@app.after_request
+def add_security_headers(response):
+    response.headers['Cache-Control'] = 'no-cache'
+    return response
--- blogapp/routes.py
+++ blogapp/routes.py
@@ -1,41 +1,39 @@
 import base64
 from flask import request, make_response, jsonify
-from blogapp import app, db, basic_auth, bcrypt
+from blogapp import app, db, bcrypt, auth
 from sqlalchemy import text
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 from blogapp.models import User
-# from flask_login import login_user, current_user
 import logging
 from datetime import datetime
 
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.DEBUG)
 
+@auth.verify_password
+def verify_password(username, password):
+
+    user = User.query.filter_by(username=username).first()
+    if user and user.check_password(password):
+        return user
+
 @app.route('/healthz', methods=['GET', 'HEAD', 'OPTIONS'])
 def health_check():
 
     if request.method in ['HEAD', 'OPTIONS']:
-        response = make_response('')
-        response.headers['Cache-Control'] = 'no-cache'
-        return response, 405
+        return make_response(''), 405
 
     if request.get_data():
-        response = make_response('')
-        response.headers['Cache-Control'] = 'no-cache'
-        return response, 400
+        return make_response(''), 400
 
     try:
         with db.engine.connect() as connection:
             connection.execute(text("SELECT 1"))
 
-        response = make_response('')
-        response.headers['Cache-Control'] = 'no-cache'
-        return response
+        return make_response('', 200)
     
     except SQLAlchemyError as e:
-        response = make_response('')
-        response.headers['Cache-Control'] = 'no-cache'
-        return response, 503
+        return make_response(''), 503
 
 @app.route('/v1/user', methods=['POST'])
 def create_user():
@@ -45,21 +43,17 @@ def create_user():
         data = request.get_json()
     except (ValueError, KeyError, TypeError):
         return make_response(''), 400
-
+    
     # validation for mandatory fields
     required_fields = ['first_name', 'last_name', 'password', 'username']
-    if not all(field in data for field in required_fields):
-        response = make_response('')
-        response.headers['Cache-Control'] = 'no-cache'
-        return response, 400
+    if not all(field in data for field in required_fields) or len(data) != len(required_fields):
+        return make_response(''), 400
 
     try:
         # validation of email address already exists for the user
         existing_user = User.query.filter_by(username=data['username']).first()
         if existing_user:
-            response = make_response('')
-            response.headers['Cache-Control'] = 'no-cache'
-            return response, 400
+            return make_response(''), 400
 
         new_user = User(
             first_name=data['first_name'],
@@ -81,34 +75,22 @@ def create_user():
             "account_updated": new_user.account_updated.isoformat()
         }
 
-        # # Response headers
-        # response_headers = {
-        #     'access-control-allow-credentials': 'true',
-        #     'access-control-allow-headers': 'X-Requested-With,Content-Type,Accept,Origin',
-        #     'access-control-allow-methods': '*',
-        #     'access-control-allow-origin': '*',
-        #     'cache-control': 'no-cache',
-        #     'content-encoding': 'gzip',
-        #     'content-type': 'application/json;charset=utf-8',
-        #     'date': datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT'),
-        #     'expires': '-1',
-        #     'server': 'nginx',
-        #     'x-powered-by': 'Express'
-        # }
-
         return make_response(jsonify(response_payload), 201)
 
     except IntegrityError as e:
         # database integrity 
         db.session.rollback()
-        return make_response(jsonify({'error': 'Database integrity error'}), 500)
+        return make_response(jsonify({'error': 'Database integrity error'}), 503)
+    except (ValueError, KeyError, TypeError):
+        db.session.rollback()
+        return make_response('', 400)
 
 @app.route('/v1/user/self', methods=['GET'])
-@basic_auth.required
+@auth.login_required
 def get_user():
     auth_header = request.headers.get('Authorization')
     if not auth_header:
-        return make_response(jsonify({'error': 'Authorization header missing'}), 401)
+        return make_response('', 401)
     
     try:
         auth_type, credentials = auth_header.split(' ')
@@ -129,80 +111,69 @@ def get_user():
                 "account_updated": user.account_updated.isoformat()
             }
 
-        return make_response(jsonify(response_payload), 200)
+            return make_response(jsonify(response_payload), 200)
 
     except ValueError:
-        pass
+        return make_response('', 404)
 
-    return make_response(jsonify({'error': 'Invalid Authorization header'}), 401)
+    return make_response('', 401)
 
 @app.route('/v1/user/self', methods=['PUT'])
-@basic_auth.required
+@auth.login_required
 def update_user_info():
 
     auth_header = request.headers.get('Authorization')
     if not auth_header:
-        return make_response(jsonify({'error': 'Authorization header missing'}), 401)
+        return make_response('', 401)
 
-    auth_type, credentials = auth_header.split(' ')
-    if auth_type.lower() == 'basic':
-        decoded_credentials = base64.b64decode(credentials).decode('utf-8')
-        username, _ = decoded_credentials.split(':', 1)
+    try:
+        data = request.get_json()
+    except (ValueError, KeyError, TypeError):
+        return make_response(''), 400
 
-        user = User.query.filter_by(username=username).first()
-        if not user:
-            return make_response(jsonify({'error': 'User not found'}), 404)
-        
-    data = request.get_json()
+    try:
+        auth_type, credentials = auth_header.split(' ')
+        if auth_type.lower() == 'basic':
+            decoded_credentials = base64.b64decode(credentials).decode('utf-8')
+            username, _ = decoded_credentials.split(':', 1)
 
-    # except (ValueError, KeyError, TypeError):
-    #     return make_response(''), 400
+            user = User.query.filter_by(username=username).first()
+            if not user:
+                return make_response(jsonify({'error': 'User not found'}), 404)
 
-    # Check if only allowed fields are being updated
-    allowed_fields = ['first_name', 'last_name', 'password']
-    if not all(field in data for field in allowed_fields) or len(data) != len(allowed_fields):
-        return make_response(jsonify({'error': 'Invalid fields for update'}), 400)
+        # Check if only allowed fields are being updated
+        allowed_fields = ['first_name', 'last_name', 'password']
+        if not all(field in data for field in allowed_fields) or len(data) != len(allowed_fields):
+            return make_response(jsonify({'error': 'Invalid fields for update'}), 400)
 
-    # Update user information
-    user.first_name = data.get('first_name', user.first_name)
-    user.last_name = data.get('last_name', user.last_name)
-    user.password = bcrypt.generate_password_hash(data.get('password', user._password)).decode('utf-8')
-    user.account_updated = datetime.utcnow()
+        # Update user information
+        user.first_name = data.get('first_name', user.first_name)
+        user.last_name = data.get('last_name', user.last_name)
+        user.password = bcrypt.generate_password_hash(data.get('password', user._password)).decode('utf-8')
 
-    # Save the updated user to the database
-    db.session.commit()
+        # Save the updated user to the database
+        db.session.commit()
 
-    # Response payload without password
-    response_payload = {
-        "id": user.id,
-        "first_name": user.first_name,
-        "last_name": user.last_name,
-        "username": user.username,
-        "account_created": user.account_created.isoformat(),
-        "account_updated": user.account_updated.isoformat()
-    }
+        # Response payload without password
+        response_payload = {
+            "id": user.id,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "username": user.username,
+            "account_created": user.account_created.isoformat()
+        }
 
-    # # Response headers
-    # response_headers = {
-    #     'access-control-allow-credentials': 'true',
-    #     'access-control-allow-headers': 'X-Requested-With,Content-Type,Accept,Origin',
-    #     'access-control-allow-methods': '*',
-    #     'access-control-allow-origin': '*',
-    #     'cache-control': 'no-cache',
-    #     'content-encoding': 'gzip',
-    #     'content-type': 'application/json;charset=utf-8',
-    #     'date': datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT'),
-    #     'expires': '-1',
-    #     'server': 'nginx',
-    #     'x-powered-by': 'Express'
-    # }
-
-    return make_response('', 204)
-
+        return make_response('', 204)
+    except IntegrityError as e:
+        # database integrity 
+        db.session.rollback()
+        return make_response(jsonify({'error': 'Database integrity error'}), 503) 
 
 
 @app.errorhandler(405)
-def handle_http_errors(error):
-    response = make_response('')
-    response.headers['Cache-Control'] = 'no-cache'
-    return response, 405
+def method_not_allowed(error):
+    return make_response(''), 405
+
+@auth.error_handler
+def authorized_access():
+    return make_response(''), 401
