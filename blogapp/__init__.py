8e16ab3f41a420ad31b8e7b0ba8559879611735b
Siddharth Dhanuka <nonsense171@gmail.com>
2024-02-28 17:30:17 -0500

    get DB URI from environment variables

--- blogapp/__init__.py
+++ blogapp/__init__.py
@@ -3,14 +3,24 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_login import LoginManager
-from flask_httpauth import HTTPBasicAuth 
+from flask_httpauth import HTTPBasicAuth
+from dotenv import load_dotenv
 import os
 
 app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/cloud'
+load_dotenv("/tmp/webapp/webapp.env")
+db_name = os.getenv("DB_NAME")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+project_id = os.getenv("PROJECT_ID")
+instance_name = os.getenv("SQL_INSTANCE")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['BCRYPT_HANDLE_LONG_PASSWORDS'] = True
 
